c0a82d7d1a46d06e9e7b545ff857c16c58c8f90e
Irgendeinwer <irgendeinwer@proton.me>
2025-01-19 23:15:58 +0100

    flake.nix init

---
+++ flake.nix
@@ -0,0 +1,60 @@
+{
+  description = "NixOS config flake.nix";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    
+    home-manager = {
+	url = "github:nix-community/home-manager";
+	inputs.nixpkgs.follows = "nixpkgs";
+    };
+    
+    nixvim = {
+	url = "github:nix-community/nixvim";
+	inputs.nixpkgs.follows = "nixpkgs";
+    };
+    
+    zen-browser.url = "github:0xc000022070/zen-browser-flake";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    nixos-hardware,
+    ... 
+  }@inputs:
+  {
+    nixosConfigurations."junixos" = nixpkgs.lib.nixosSystem {
+	specialArgs = {inherit inputs;};
+	modules = [
+	    ./hosts/junixos/configuration.nix
+	    
+	    inputs.home-manager.nixosModules.default
+	    {
+		home-manager.useGlobalPkgs = true;
+            	home-manager.useUserPackages = true;
+            }
+
+	    inputs.nixvim.nixosModules.nixvim
+	];
+    };
+
+    nixosConfigurations."junixbook" = nixpkgs.lib.nixosSystem {
+	specialArgs = {inherit inputs;};
+	modules = [
+	    ./hosts/junixbook/configuration.nix
+
+	    nixos-hardware.nixosModules.common-cpu-intel
+	    nixos-hardware.nixosModules.common-pc-ssd
+
+	    inputs.home-manager.nixosModules.default
+	    {
+		home-manager.useGlobalPkgs = true;
+		home-manager.useUserPackages = true;
+	    }
+
+	    inputs.nixvim.nixosModules.nixvim
+	];
+    };
+  };
+}
